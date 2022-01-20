1bfb2cecc1afee0925710edf2552522a84aa92d1
MxLourier <87769104+MxLourier@users.noreply.github.com>
2022-01-20 20:32:25 +0200

    Version 2 - add, edit, delete pages

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-
 var mysql = require("mysql");
 var express = require("express");
 const bodyParser = require("body-parser");
-var ejs = require ("ejs");
+var ejs = require("ejs");
 
 var posts = [];
 var length;
 var length2;
-const app=express();
+const app = express();
 
 
 app.set('view engine', 'ejs'); //???
+
+app.use(express.urlencoded({
+  extended: true
+}))
 
 var con = mysql.createConnection({
   host: "localhost",
@@ -20,33 +23,25 @@ var con = mysql.createConnection({
   database: "blog"
 });
 
-// con.connect(function(err){
-//   if (err) throw err;
-//   console.log("Connected!");
-//   con.query("SELECT * FROM `pages`", function(err, result, fields){
-//     if (err) throw err;
-//     //console.log(result);
-//     posts=result;
-//     console.log(posts);
-//     length = posts.length;
-//     })
-// });
 
-con.connect(function(err){
+con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
 });
 
-app.get("/", function(req, res){
+app.get("/", function(req, res) {
   console.log("New DB query!");
-  con.query("SELECT * FROM `pages`", function(err, result, fields){
+  con.query("SELECT * FROM `pages`", function(err, result, fields) {
     if (err) throw err;
     //console.log(result);
-    posts=result;
+    posts = result;
     console.log(posts);
     length = posts.length;
-    })
-  res.render("home", {posts:posts, length: length})
+  })
+  res.render("home", {
+    posts: posts,
+    length: length
+  })
 })
 
 // app.get("/posts/:topic", function(req, res){
@@ -60,24 +55,92 @@ app.get("/", function(req, res){
 //
 // })
 
-app.get("/posts/:topic", function(req,res){
-  var match=false;
-  posts.forEach(function(element){
-    if(element.url===("/"+req.params.topic)){
-      match=true;
-      con.query("SELECT * FROM `paragraphs` WHERE `paragraphs`.`PageID` = " +element.ID, function(err, result, fields){
+app.get("/posts/:topic", function(req, res) {
+  var match = false;
+  posts.forEach(function(element) {
+    if (element.url === ("/" + req.params.topic)) {
+      match = true;
+      con.query("SELECT * FROM `paragraphs` WHERE `paragraphs`.`PageID` = " + element.ID, function(err, result, fields) {
         if (err) throw err;
         //console.log(result);
-        var paragraphs=result;
+        var paragraphs = result;
         console.log(paragraphs);
         length2 = paragraphs.length;
-        res.render("post", {postTitle: element.Title, postBody:element.Summary, content:paragraphs, length: length2})
+        res.render("post", {
+          postTitle: element.Title,
+          postBody: element.Summary,
+          content: paragraphs,
+          length: length2,
+          id: element.ID
         })
+      })
 
     }
   });
   if (!match) {
-    res.render("post", {postTitle: "Not found", postBody:"No post found for "+req.params.topic})
+    res.render("post", {
+      postTitle: "Not found",
+      postBody: "No post found for " + req.params.topic,
+      id: 0
+    })
+  }
+})
+
+app.get("/admin/post/:id/delete", function(req, res) {
+  var match = false;
+  posts.forEach(function(element, index) {
+    if (element.ID == req.params.id) {
+      match = true;
+      con.query("DELETE FROM `paragraphs` WHERE `paragraphs`.`PageID` = " + element.ID, function(err) {
+        if (err) throw err;
+        //console.log(result);
+        //var paragraphs=result;
+        con.query("DELETE FROM `pages` WHERE `ID` = " + element.ID, function(err) {
+          if (err) throw err;
+          //posts.splice(index,1);
+          res.redirect("/");
+        })
+
+      })
+
+    }
+  });
+  if (!match) {
+    res.render("post", {
+      postTitle: "Not found",
+      postBody: "No post found for " + req.params.topic,
+      id: 0
+    })
+  }
+})
+
+app.get("/admin/post/:id/edit", function(req,res){
+  if(req.params.id==0){
+      res.render("edit", {post: 0});
+  }else{
+    con.query("SELECT * FROM `pages` WHERE `ID` = " + req.params.id, function(err, result, fields){
+      if (err) throw err;
+      console.log(result);
+        res.render("edit", {post: result[0]});
+     })
+  }
+
+
+})
+
+app.post("/admin/post/:id/edit", function(req,res){
+  if (req.params.id==0){
+    con.query("INSERT INTO `pages` (`url`, `Title`, `Summary`) VALUES ('" + req.body.url + "', '"+ req.body.title+"', '" + req.body.summary +"');", function(err, result){
+      if(err) throw err;
+      res.redirect("/");
+
+    })
+
+  }else{
+    con.query("UPDATE `pages` SET `url` = '" + req.body.url + "', `Title` = '" + req.body.title + "', `Summary` = '" + req.body.summary + "' WHERE `pages`.`ID` =" + req.params.id + ";", function(err){
+      if(err) throw err;
+      res.redirect("/");
+    })
   }
 })
 
